9462abbf5a2f6c93bb7366b1a04cb0314979add6
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2023-04-23 02:25:35 +0530

    Added MenuItem in UserMenu in Navbar

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,6 +2,7 @@
 
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
   return (
@@ -23,6 +24,14 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={() => {}} label="Sign Up" />
+            </>
+          </div>
+        </div>
     </div>
   );
 };
